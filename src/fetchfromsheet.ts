2ccc7ab0e6365de70d0a092142c691ab368084e8
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2023-03-13 16:39:57 -0700

    hash key to prevent constant writing to db

--- src/fetchfromsheet.ts
+++ src/fetchfromsheet.ts
@@ -13,6 +13,16 @@ import {pgclient} from './postgres'
 import {google} from "googleapis";
 const sheets = google.sheets("v4");
 
+const simpleHash = (str:string) => {
+  let hash = 0;
+  for (let i = 0; i < str.length; i++) {
+    const char = str.charCodeAt(i);
+    hash = (hash << 5) - hash + char;
+    hash &= hash; // Convert to 32bit integer
+  }
+  return new Uint32Array([hash])[0].toString(36);
+};
+
 const cors = require("cors")({origin: true});
 // const _ = require("lodash");
 // import DatadogWinston from 'datadog-winston'
@@ -129,98 +139,138 @@ async function fetchSheet() {
  // read rows
 const rows = await sheet.getRows();
 
+const hashofrows = simpleHash(JSON.stringify(rows));
+
+let letthrough = true;
+
+const checkiftableexistsreq = `SELECT EXISTS (
+  SELECT FROM 
+      information_schema.tables 
+  WHERE 
+      table_schema LIKE 'public' AND 
+      table_type LIKE 'BASE TABLE' AND
+      table_name = 'shelterhashinfo'
+  )`
+
+  const checkiftableexists = await pgclient.query(checkiftableexistsreq, [])
+
+  console.log(checkiftableexists.rows[0].exists);
+
+if (checkiftableexists?.rows[0]?.exists) {
+  const checkhash = await pgclient.query(`SELECT * FROM shelterhashinfo WHERE name = 'main'`, [])
+
+  if (checkhash?.rows[0]?.hash === hashofrows) {
+    console.log('hashes match, not updating')
+    letthrough = false;
+  }
+}
+
+if (letthrough === true) {
+  
 //console.log(rows);
 console.log('rows', rows.length)
 
-    const arrayOfRowsInsertsToPerform:Array<any> = []
+const arrayOfRowsInsertsToPerform:Array<any> = []
 
-  for (const row of rows) {
+for (const row of rows) {
 
-    if (row.lat != null && row.lng != null && row.lat != "" && row.lng != "") {
+if (row.lat != null && row.lng != null && row.lat != "" && row.lng != "") {
 
-      if (!Number.isNaN(row.lat) && !Number.isNaN(row.lng)) {
-        
-    console.log(row);
+  if (!Number.isNaN(row.lat) && !Number.isNaN(row.lng)) {
+    
+console.log(row);
 
-    console.log(row.lat);
+console.log(row.lat);
 
-      arrayOfRowsInsertsToPerform.push(pgclient.query(`INSERT INTO ${generatenameoftable} 
-      (lat,
-        lng,
-        cd,
-        spa,
-        organization_name,
-        projectname,
-        address,
-        type,
-        total_beds,
-        male_available,
-        female_available,
-        beds_available,
-        last_updated,
-        criteria,
-        contact_info,
-        website,
-        waiting_list,
-        iswaitlist,
-        isnodata
-        ) VALUES (
-          $1,
-          $2,
-          $3,
-          $4,
-          $5,
-          $6,
-          $7,
-          $8,
-          $9,
-          $10,
-          $11,
-          $12,
-          $13,
-          $14,
-          $15,
-          $16,
-          $17,
-          $18,
-          $19
-        )`, [
-        row.lat,
-        row.lng,
-        row.cd,
-        row.spa,
-        row.organization_name,
-        row.projectname,
-        row.address,
-        row.type,
-        cleannumber(row.total_beds),
-        cleannumber(row.male_available),
-        cleannumber(row.female_available),
-        cleannumber(row.beds_available),
-        cleandate(row.last_updated),
-        row.criteria,
-        row.contact_info,
-        row.website,
-        cleannumber(row.waiting_list),
-        cleanbool(row.iswaitlist),
-        cleanbool(row.isnodata)
-        ]))    
-      } else {
-        console.log("skipped row because lat or lng is not a number", row.lat, row.lng)
-      }
-
-    } else {
-      console.log("skipped row because lat or lng is not a number outer loop", row.lat, row.lng)
-    }
+  arrayOfRowsInsertsToPerform.push(pgclient.query(`INSERT INTO ${generatenameoftable} 
+  (lat,
+    lng,
+    cd,
+    spa,
+    organization_name,
+    projectname,
+    address,
+    type,
+    total_beds,
+    male_available,
+    female_available,
+    beds_available,
+    last_updated,
+    criteria,
+    contact_info,
+    website,
+    waiting_list,
+    iswaitlist,
+    isnodata
+    ) VALUES (
+      $1,
+      $2,
+      $3,
+      $4,
+      $5,
+      $6,
+      $7,
+      $8,
+      $9,
+      $10,
+      $11,
+      $12,
+      $13,
+      $14,
+      $15,
+      $16,
+      $17,
+      $18,
+      $19
+    )`, [
+    row.lat,
+    row.lng,
+    row.cd,
+    row.spa,
+    row.organization_name,
+    row.projectname,
+    row.address,
+    row.type,
+    cleannumber(row.total_beds),
+    cleannumber(row.male_available),
+    cleannumber(row.female_available),
+    cleannumber(row.beds_available),
+    cleandate(row.last_updated),
+    row.criteria,
+    row.contact_info,
+    row.website,
+    cleannumber(row.waiting_list),
+    cleanbool(row.iswaitlist),
+    cleanbool(row.isnodata)
+    ]))    
+  } else {
+    console.log("skipped row because lat or lng is not a number", row.lat, row.lng)
   }
 
-  await Promise.all(arrayOfRowsInsertsToPerform);
+} else {
+  console.log("skipped row because lat or lng is not a number outer loop", row.lat, row.lng)
+}
+}
 
-  console.log("done inserting to temp table");
+await Promise.all(arrayOfRowsInsertsToPerform);
 
-  const dropoldtable = await pgclient.query(`BEGIN; DROP TABLE IF EXISTS shelters; ALTER TABLE ${generatenameoftable} RENAME TO shelters; COMMIT;`, [])
+console.log("done inserting to temp table");
 
-  console.log("done renaming temp table to shelters");
+const dropoldtable = await pgclient.query(`BEGIN; DROP TABLE IF EXISTS shelters; ALTER TABLE ${generatenameoftable} RENAME TO shelters; COMMIT;`, [])
+
+//make table if not exists
+const maketable = await pgclient.query(`CREATE TABLE IF NOT EXISTS shelterhashinfo (
+  name text PRIMARY KEY,
+  hash text,
+  timeofwrite timestamp
+)`, [])
+
+//write hash to db
+const writehash = await pgclient.query(`INSERT INTO shelterhashinfo (name, hash, timeofwrite) VALUES ('main', $1, $2) ON CONFLICT (name) DO UPDATE SET hash = $1`, [hashofrows, Date.now()])
+
+console.log("done renaming temp table to shelters");
+}
+
 
 }
 
